2295cec720403564a2094271bdc34ed13ff652fc
tinaz707 <csm347@drexel.edu>
2026-03-15 21:58:31 -0400

    💄💫🔨Add Scripts + Update Side Bar Style
    Info Change + Audio + Calendar Input Update (Script)
    → each button updates with related zodiac information + audio plays
    → The calendar form works similarly to the button click
    → function 'handleform' repeats audio information to play audio correctly
    → update album covers so each zodiac has a different one
    → update CSS for side-bar-info to let it take the middle of the screen and use the 'aside' class to give the background a blur

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,7 @@ const zodiacSigns = [
         sound: "Content/Music/sagittrius_Tonight.mp3",
         image: "Content/Images/Album/tonight_album.webp",
         album: "from Fancy That",
-        text: "Born from November 22nd to December 21st, Sagittarius people are on top of the world! We are the subjects, and they are the leader. They aren’t afraid to dive headfirst into the unknown.. They truly are free spirits, nothing holds them back! With so much enthusiasm, it can be hard to truly focus on one thing at a time.",
-        
+        text: "Born from November 22nd to December 21st, Sagittarius people are on top of the world! We are the subjects, and they are the leader. They aren’t afraid to dive headfirst into the unknown. They truly are free spirits, nothing holds them back! With so much enthusiasm, it can be hard to truly focus on one thing at a time.",
     },
 
     {
@@ -15,14 +14,14 @@ const zodiacSigns = [
         sound: "Content/Music/capricorn_CapableOfLove.mp3",
         image: "Content/Images/Album/heavenKnows_album.webp",
         album: "from Heaven Knows",
-        text: "Born from December 22nd to January 19th, Capricorns are truly the responsible ones in every friend group. They are independent and know how to manage just about anyone. All they need is your respect and to be able to prove themselves. Its no surprise their biggest rival is themselves!",
+        text: "Born from December 22nd to January 19th, Capricorns are truly the responsible ones in every friend group. They are independent and know how to manage just about anyone. All they need is your respect and to be able to prove themselves. It's no surprise their biggest rival is themselves!",
     },
 
     {
         id: "Aquarius",
         song:"Feel Complete",
         sound: "Content/Music/Aquarius_feelcomplete.mp3",
-        image: "Content/Images/Album/heavenKnows_album.webp",
+        image: "Content/Images/Album/fell_complete_album.webp",
         album: "from Heaven Knows",
         text: "Born from January 22nd to February 18th, Aquarius can be both shy and energetic. Being boring isn’t in their dictionary, it's in them to live as the opposite of everyone else around them. They define themselves rather than having the world plaster a definition on them!",
     },
@@ -30,7 +29,7 @@ const zodiacSigns = [
         id: "Pisces",
         song:"Take Me Home",
         sound: "Content/Music/Pisces_Take_Me_home.mp3",
-        image: "Content/Images/Album/ToHellWithIt_album.webp",
+        image: "Content/Images/Album/take_me_home_album.webp",
         album: "from To Hell With It",
         text: "Born from February 19th to March 20th, Pisces are a very friendly bunch. They adapt their personality to whomever they find themselves with! They can’t stick to one form but want to become one with everything around them. Regardless of it all, they are very charming and compassionate!",
     },
@@ -48,7 +47,7 @@ const zodiacSigns = [
         sound: "Content/Music/virgo_DoYouMissMe.mp3",
         image: "Content/Images/Album/miss_me_albu.webp",
         album: "Take Me Home",
-        text: "Born from August 23rd to September 22nd, Virgos are your one and only perfectionists. They don’t mind giving a helping hand, but that does sometimes mean they put others first rather than themselves. You can alway rely to get good advice, but be careful, they can be a little judgmental!",
+        text: "Born from August 23rd to September 22nd, Virgos are your one and only perfectionists. They don’t mind giving a helping hand, but that does sometimes mean they put others first rather than themselves. You can always rely on them to give good advice, but be careful, they can be a little judgmental!",
     },
         {
         id: "Libra",
@@ -62,23 +61,23 @@ const zodiacSigns = [
         id: "Scorpio",
         song:"Feelings",
         sound: "Content/Music/scorpio_Feelings.mp3",
-        image: "Content/Images/Album/heavenKnows_album.webp",
+        image: "Content/Images/Album/feelings_album.webp",
         album: "from Heaven Knows",
-        text: "Born from October 23rd to November 21st, Scorpios can be intimidating at first, but they are very passionate and trustworthy. It can be hard to get to know them, but it's possible! Slowly, as they get comfortable, they let (most) of their walls down. Almost like their mystical…magical",
+        text: "Born from October 23rd to November 21st, Scorpios can be intimidating at first, but they are very passionate and trustworthy. It can be hard to get to know them, but it's possible! Slowly, as they get comfortable, they let (most) of their walls down. Almost like they’re mystical…magical",
     },
         {
         id: "Aries",
         song:"Stateside",
         sound: "Content/Music/areis_stateside.mp3",
-        image: "Content/Images/Album/fancyThat_album.webp",
+        image: "Content/Images/Album/stateside_album.webp",
         album: "from Fancy That",
-        text: "Born from March 20th to April 19th, Aries are confident and motivating people! They don’t really let anything hold them back and aren’t afraid to try anything new. (That could mean flying abroad just to meet someone new!",
+        text: "Born from March 20th to April 19th, Aries are confident and motivating people! They don’t really let anything hold them back and aren’t afraid to try anything new. That could mean flying abroad just to meet someone new!",
     },
         {
         id: "Taurus",
         song:"Nice To Meet You",
         sound: "Content/Music/tarus_NiceToMeetYou.mp3",
-        image: "Content/Images/Album/heavenKnows_album.webp",
+        image: "Content/Images/Album/nice_to_album.webp",
         album: "from Heaven Knows",
         text: "Born from April 20 to May 20, Taurus are wherever they find comfort and relaxation.  They appreciate stability and meaningful connections, so don’t be afraid to mingle with them! As loyal and fun as they can be, don’t catch them on a bad day, or you might just get caught up in their explosion!",
     },
@@ -86,17 +85,17 @@ const zodiacSigns = [
         id: "Gemini",
         song:"The Aisle",
         sound: "Content/Music/gemini_TheAisle.mp3",
-        image: "Content/Images/Album/heavenKnows_album.webp",
+        image: "Content/Images/Album/aisle_album.webp",
         album: "from Heaven Knows",
-        text: "Born from May 21st to June 20th, Geminis are charismatic, sociable, and ready for any kind of fun. They are active and curious individuals, so you'd better keep up! Don’t underestimate them, even if they seem to be joking around always, they can be quick-witted",
+        text: "Born from May 21st to June 20th, Geminis are charismatic, sociable, and ready for any kind of fun. They are active and curious individuals, so you'd better keep up! Don’t underestimate them! Even if they seem to be joking around, they can be quick-witted",
     },
         {
         id: "Cancer",
         song:"Mosquito",
         sound: "Content/Music/cancer_Mosquito.mp3",
-        image: "Content/Images/Album/heavenKnows_album.webp",
+        image: "Content/Images/Album/mosquito_album.webp",
         album: "from Heaven Knows",
-        text: "Born from June 21st to July 22nd, Cancers are on the more of the sensitive side, so watch what you say! They take things seriously and care deeply about many things.. They require that comfort and stability, straying away from sudden changes!",
+        text: "Born from June 21st to July 22nd, Cancers are on the more of the sensitive side, so watch what you say! They take things seriously and care deeply about many things. They require  comfort and stability, avoiding from sudden changes!",
     },
 ];
 
@@ -188,7 +187,7 @@ if (birthdayForm){
 //---------------------------------------
 /*side bar details*/
 const exitSide = document.querySelector('.exit-button');
-const sidebarOverlay = document.querySelector('.side-bar-info'); /*gets my sidebar*/
+const sidebarOverlay = document.querySelector('.side-bar-open'); /*gets my sidebar*/
 const zType = document.getElementById('z-name');
 const zAlbum = document.getElementById('z-album-image');
 const zSongName = document.getElementById('z-song');
@@ -260,7 +259,7 @@ function playSound(sound){ //for each button
 //---------------------------------------
 //updates
 function updateUi(sign){
-    zType.innerText = sign.id //
+    zType.innerText = sign.id//
     zAlbum.src = sign.image 
     zSongName.innerText = sign.song
     zAlbumName.innerText = sign.album
